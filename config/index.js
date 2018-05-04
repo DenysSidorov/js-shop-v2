2f4bdbe5b6788e779c1d0edc87f89abd5822c8df
Denys Sidorov <000scorpions000@gmail.com>
2018-05-04 23:36:23 +0300

    add config file

---
+++ config/index.js
@@ -0,0 +1,21 @@
+var configApp = {
+    frontend: {
+        // port: 8090,
+        // domain: 'localhost',
+        // apiPort: 3000 // 3009
+    },
+    backend: {
+        port: process.env.PORT, //5005
+        // database: process.env.MONGODB_URI, // 'mongodb://localhost:27017/shop'
+        // domain: process.env.SERVER_DOMAIN,
+        // secretWord: 'verysecretkey', //process.env.SECRET_WORD, // 'verysecretkey'
+        // mailSend: {
+        //     service: "Gmail",
+        //     auth: {
+        //         user: process.env.EMAIL_USER, // "1qazxsw,
+        //         pass: process.env.EMAIL_PASSWORD //
+        //     }
+        // },
+    },
+};
+module.exports = configApp;
