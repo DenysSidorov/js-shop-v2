85e4ca034c34121318b166b115372b194dca326d
Denys Sidorov <000scorpions000@gmail.com>
2018-06-04 23:39:08 +0300

    remove unusable code

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,12 +1,2 @@
-// import slick from '../../components/slick/slick'
 import './react_bla.scss';
-// import './jop.less';
-// import 'normalize.css';
-
-// import createMenu from '../../components/menu/menu';
-// var menu = createMenu(['Главная!/*','Блог'], 'menu');
-// document.body.appendChild(menu);
-
 console.log('in index2.js');
-// console.log($);
-// console.log(jQuery);
